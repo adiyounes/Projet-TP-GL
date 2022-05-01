d70d81eb28672d4300544b3d1b48ad170e14f9f5
[YOUNES ADI] <younesadi18@gmail.com>
2022-05-01 04:57:23 +0100

    hello-git-modify

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -18,7 +18,8 @@ public class JavaApplication8 {
      */
     public static void main(String[] args) {
       System.out.print("HI HI");
-        System.out.print("NOOOOO"):
+        System.out.print("NOOOOO");
+        System.out.print("yeeees");
     }
     
 }
